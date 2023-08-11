46e1ffeafc8f1f76e85ce11e97d1e1f9324f97e1
yruej301 <jeurymejia301@gmail.com>
2023-08-11 18:08:43 -0400

    Improve typing

--- vertex_protocol/utils/asserts.py
+++ vertex_protocol/utils/asserts.py
@@ -1,4 +1,9 @@
-def assert_book_not_empty(bids: list, asks: list, is_bid: bool):
+from typing import Tuple
+
+
+def assert_book_not_empty(
+    bids: list[Tuple[str, str]], asks: list[Tuple[str, str]], is_bid: bool
+):
     book_is_empty = (is_bid and len(bids) == 0) or (not is_bid and len(asks) == 0)
     if book_is_empty:
         raise Exception("Orderbook is empty.")
