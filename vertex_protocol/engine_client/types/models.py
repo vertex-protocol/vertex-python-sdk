5b402a7eecc354fcdca1540d4cd3b5ddacd4045d
clarkohw <clark.ohw@gmail.com>
2023-08-11 14:46:34 -0700

    remove short magin info

--- vertex_protocol/engine_client/types/models.py
+++ vertex_protocol/engine_client/types/models.py
@@ -186,9 +186,6 @@ class SymbolData(VertexBaseModel):
     taker_fee_rate_x18: str
     long_weight_initial_x18: str
     long_weight_maintenance_x18: str
-    short_weight_initial_x18: str
-    short_weight_maintenance_x18: str
-
 
 class SubaccountPosition(VertexBaseModel):
     balance: Union[PerpProductBalance, SpotProductBalance]
