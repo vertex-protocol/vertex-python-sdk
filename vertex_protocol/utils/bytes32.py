1daaf3a4b519e584ee46c480bfd6f5b38619975b
yruej301 <jeurymejia301@gmail.com>
2023-09-28 14:25:57 -0400

    ...

--- vertex_protocol/utils/bytes32.py
+++ vertex_protocol/utils/bytes32.py
@@ -1,6 +1,23 @@
 import binascii
 from typing import Optional, Union
-from vertex_protocol.utils.subaccount import Subaccount, SubaccountParams
+
+from vertex_protocol.utils.model import VertexBaseModel
+
+
+class SubaccountParams(VertexBaseModel):
+    """
+    A class used to represent parameters for a Subaccount in the Vertex system.
+
+    Attributes:
+        subaccount_owner (Optional[str]): The wallet address of the subaccount.
+        subaccount_name (str): The subaccount name identifier.
+    """
+
+    subaccount_owner: Optional[str]
+    subaccount_name: str
+
+
+Subaccount = Union[str, bytes, SubaccountParams]
 
 
 def hex_to_bytes32(input: Union[str, bytes]) -> bytes:
