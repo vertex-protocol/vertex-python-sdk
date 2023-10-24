65ace309e65210d4491008f7a04569af96f838ef
yruej301 <jeurymejia301@gmail.com>
2023-10-23 23:29:50 -0400

    update backend endpoints

--- vertex_protocol/utils/backend.py
+++ vertex_protocol/utils/backend.py
@@ -4,7 +4,7 @@ from vertex_protocol.utils.enum import StrEnum
 class VertexBackendURL(StrEnum):
     """Enum representing different Vertex backend URLs."""
 
-    TESTNET = "https://test.vertexprotocol-backend.com"
-    MAINNET = "https://prod.vertexprotocol-backend.com"
+    TESTNET = "https://api.test.vertexprotocol.com"
+    MAINNET = "https://api.prod.vertexprotocol.com"
     DEVNET_ENGINE = "http://localhost:80"
     DEVNET_INDEXER = "http://localhost:8000"
