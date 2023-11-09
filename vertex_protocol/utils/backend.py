2ee2fb86eb94adf469b5d16740a98a32f39d5e1a
yruej301 <jeurymejia301@gmail.com>
2023-11-08 22:57:12 -0500

    Support arbi sepolia

--- vertex_protocol/utils/backend.py
+++ vertex_protocol/utils/backend.py
@@ -4,6 +4,7 @@ from vertex_protocol.utils.enum import StrEnum
 class VertexBackendURL(StrEnum):
     """Enum representing different Vertex backend URLs."""
 
+    SEPOLIA_TESTNET = "https://api.sepolia-test.vertexprotocol.com"
     TESTNET = "https://api.test.vertexprotocol.com"
     MAINNET = "https://api.prod.vertexprotocol.com"
     DEVNET_ENGINE = "http://localhost:80"
