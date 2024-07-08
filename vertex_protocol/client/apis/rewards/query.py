94f908fcf1eb620207fee8805ad2d94b9576891b
yruej301 <jeurymejia301@gmail.com>
2024-07-08 18:29:00 -0500

    claim and stake estimated vrtx is functional

--- vertex_protocol/client/apis/rewards/query.py
+++ vertex_protocol/client/apis/rewards/query.py
@@ -2,8 +2,11 @@ from vertex_protocol.client.apis.base import VertexBaseAPI
 
 
 class RewardsQueryAPI(VertexBaseAPI):
-    def get_claim_and_stake_estimated_vrtx(self, wallet: str):
+    def get_claim_and_stake_estimated_vrtx(self, wallet: str) -> int:
         """
         Estimates the amount of USDC -> VRTX swap when claiming + staking USDC rewards
         """
-        pass
+        assert self.context.contracts.vrtx_staking is not None
+        return self.context.contracts.vrtx_staking.functions.getEstimatedVrtxToStake(
+            wallet
+        ).call()
