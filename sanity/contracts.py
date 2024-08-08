e7b9d8e0a6d2d24c2f0d6779a521dee205fa180b
yruej301 <jeurymejia301@gmail.com>
2024-08-08 14:31:11 -0400

    update sanity scripts

--- sanity/contracts.py
+++ sanity/contracts.py
@@ -10,9 +10,7 @@ def run():
         node_url=deployment.node_url,
         contracts_context=VertexContractsContext(**deployment.dict()),
     )
-    wallet = vertex_contracts.w3.to_checksum_address(
-        "0xcb60ca32b25b4e11cd1959514d77356d58d3e138"
-    )
+
     print("node url:", deployment.node_url)
     print("endpoint:", vertex_contracts.endpoint.address)
     print("querier:", vertex_contracts.querier.address)
@@ -20,6 +18,10 @@ def run():
     print("spot_engine:", vertex_contracts.spot_engine.address)
     print("perp_engine:", vertex_contracts.perp_engine.address)
     print("n-submissions", vertex_contracts.endpoint.functions.nSubmissions().call())
-    print(
-        "getClaimed", vertex_contracts.vrtx_airdrop.functions.getClaimed(wallet).call()
-    )
+
+    # wallet = vertex_contracts.w3.to_checksum_address(
+    #     "0xcb60ca32b25b4e11cd1959514d77356d58d3e138"
+    # )
+    # print(
+    #     "getClaimed", vertex_contracts.vrtx_airdrop.functions.getClaimed(wallet).call()
+    # )
