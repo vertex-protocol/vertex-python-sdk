3a6963dee4dd31b8b431e3fd93150462970f4d71
yruej301 <jeurymejia301@gmail.com>
2024-09-05 13:07:27 -0400

    add place order sig sample

--- sanity/signing.py
+++ sanity/signing.py
@@ -8,6 +8,18 @@ from vertex_protocol.contracts.eip712.sign import (
 from vertex_protocol.client import VertexClient, create_vertex_client
 
 
+import time
+
+from vertex_protocol.engine_client.types.execute import (
+    OrderParams,
+)
+
+from vertex_protocol.utils.expiration import OrderType, get_expiration_timestamp
+from vertex_protocol.utils.math import to_pow_10, to_x18
+from vertex_protocol.utils.nonce import gen_order_nonce
+from vertex_protocol.utils.subaccount import SubaccountParams
+
+
 def run():
     print("setting up vertex client...")
     client: VertexClient = create_vertex_client(CLIENT_MODE, SIGNER_PRIVATE_KEY)
@@ -32,3 +44,20 @@ def run():
         typed_data=authenticate_stream_typed_data, signer=client.context.signer
     )
     print("authenticate stream signature:", authenticate_stream_signature)
+
+    print("building order signature...")
+    order = OrderParams(
+        sender=SubaccountParams(
+            subaccount_owner=client.context.signer.address, subaccount_name="default"
+        ),
+        priceX18=to_x18(60000),
+        amount=to_pow_10(1, 17),
+        expiration=get_expiration_timestamp(OrderType.DEFAULT, int(time.time()) + 40),
+        nonce=gen_order_nonce(),
+    )
+    now = time.time()
+    signature = client.context.engine_client._sign(
+        "place_order", order.dict(), product_id=1
+    )
+    elapsed_time = (time.time() - now) * 1000
+    print("place order signature:", signature, "elapsed_time:", elapsed_time)
